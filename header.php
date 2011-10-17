240c8202c72610cff828d03b3237444cce31ead5
Mattia Trapani <zupolgec@gmail.com>
2011-10-18 00:30:46 +0300

    Removed loading of jQuery 1.5.2 as it's already loaded by WP

--- header.php
+++ header.php
@@ -33,6 +33,13 @@
 
 		<link href="<?php bloginfo('template_directory'); ?>/js/google-code-prettify/prettify.css" rel="stylesheet">
 
+<<<<<<< HEAD
+=======
+    <script src="http://autobahn.tablesorter.com/jquery.tablesorter.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/js/google-code-prettify/prettify.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/js/application.js"></script>
+
+>>>>>>> Removed loading of jQuery 1.5.2 as it's already loaded by WP
 <?php
 	/* We add some JavaScript to pages with the comment form
 	 * to support sites with threaded comments (when in use).
