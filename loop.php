aea719ba2c69642bf94b5f78d1f1306e10543cc5
Andrea Giannangelo <andrea@iubenda.com>
2012-01-22 10:26:02 +0100

    Fixing the social buttons
    Code updated for Twitter.
    Unsafe javascript warning coming from the G+ button can safely be ignored.

--- loop.php
+++ loop.php
@@ -136,10 +136,13 @@ if ( ( is_paged() == false && $sa_settings['compact_homepage'] == '1' ) || ( $sa
 						if( $sa_settings['social_buttons'] == '1' ) : 
 					?>
 						<div class="post_social_buttons">
-							<iframe src="http://www.facebook.com/plugins/like.php?app_id=124765724272783&amp;href=<?php the_permalink(); ?>&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=lucida+grande&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:120px; height:20px;" allowTransparency="true"></iframe>
-							<a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-count="horizontal">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
 
-							<div class="g-plusone" data-size="medium" data-href="<?php the_permalink(); ?>"></div>
+							<iframe src="http://www.facebook.com/plugins/like.php?app_id=124765724272783&amp;href=<?php the_permalink(); ?>&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=lucida+grande&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:120px; height:20px;" allowTransparency="true"></iframe>
+							
+							<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-count="horizontal">Tweet</a>
+							<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+							<g:plusone size="medium" href="<?php the_permalink(); ?>"></g:plusone>
 							<script type="text/javascript">
 							  (function() {
 							    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
@@ -147,6 +150,7 @@ if ( ( is_paged() == false && $sa_settings['compact_homepage'] == '1' ) || ( $sa
 							    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 							  })();
 							</script>
+
 						</div>
 					<?php endif; ?>
 
